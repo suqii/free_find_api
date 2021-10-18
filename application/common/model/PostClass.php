86b7fc1204c916a6c5de750d7b3e671d8076c5d1
wukui <3128006406@qq.com>
2021-10-18 14:35:59 +0800

    fix: '获取分类增加pic字段"

--- application/common/model/PostClass.php
+++ application/common/model/PostClass.php
@@ -8,7 +8,7 @@ class PostClass extends Model
 {
     // 获取所有文章分类
     public function getPostClassList(){
-        return $this->field('id,classname')->where('status',1)->select();
+        return $this->field('id,classname,classpic')->where('status',1)->select();
     }
 
     // 关联文章模型
